7317ca8574b00acba38221448de3ee3ceb0b46ae
woduf2720 <pjy5602@gmail.com>
2024-01-17 15:11:25 +0900

    swagger 적용 테스트 springdoc 사용
    530p까지 진행

---
+++ src/main/java/org/zerock/b01/dto/ReplyDTO.java
@@ -0,0 +1,30 @@
+package org.zerock.b01.dto;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyDTO {
+
+    private Long rno;
+
+    @NotNull
+    private Long bno;
+
+    @NotEmpty
+    private String replyText;
+
+    @NotEmpty
+    private String replyer;
+
+    private LocalDateTime regDate, modDate;
+}
